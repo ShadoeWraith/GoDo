5997a7d08dc1532d97e83d5655c5b69dabbbcc06
Shaun Kiszonas <shadoewraith99@gmail.com>
2025-11-23 01:02:32 -0800

    quickly added Auth0

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,7 +4,19 @@ import App from "./App.vue";
 import "./assets/styles/main.scss";
 import router from "./router";
 
+import { createAuth0 } from "@auth0/auth0-vue";
+
 const app = createApp(App);
+
+app.use(
+  createAuth0({
+    domain: import.meta.env.VITE_AUTH0_DOMAIN,
+    clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
+    authorizationParams: {
+      redirect_uri: window.location.origin,
+    },
+  })
+);
 
 app.use(router);
 app.use(createPinia());
